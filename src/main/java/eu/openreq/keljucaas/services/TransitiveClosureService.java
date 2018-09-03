b0c27686d0dd1e18b475a6dfe3cad69638c31f40
Kettunen <ejkettun@lm7-cs-41.pc.helsinki.fi>
2018-09-03 15:42:17 +0300

    modified controller, minor null checks

--- src/main/java/eu/openreq/keljucaas/services/TransitiveClosureService.java
+++ src/main/java/eu/openreq/keljucaas/services/TransitiveClosureService.java
@@ -22,126 +22,129 @@ import fi.helsinki.ese.murmeli.Relationship;
  * 
  */
 public class TransitiveClosureService {
-	
+
 	public Map<String, List<ElementRelationTuple>> generateGraph(Collection<ElementModel> models) {
-		
+
 		Map<String, List<ElementRelationTuple>> graph = new HashMap();
 		Set<String> mocks = new HashSet();
-		
+
 		for (ElementModel model : models) {
 			relationsToGraph(graph, model, mocks);
 			partsToGraph(graph, model, mocks);
 		}
-		
+
 		dealWithDuplicatingMocksInTheGraph(graph, mocks);
-		
+
 		return graph;
 	}
-	
-	
+
 	public void addAttributesToTransitiveClosure(Collection<ElementModel> models, ElementModel transitiveClosure) {
-		
+
 		for (Element element : transitiveClosure.getElements().values()) {
-			for (Integer attribute : element.getAttributes().values()) {
-				for (ElementModel model : models) {
-					if (model.getAttributeValues().containsKey(attribute)) {
-						transitiveClosure.addAttriputeValue(model.getAttributeValues().get(attribute));
+			if (element.getAttributes().values() != null) {
+				for (Integer attribute : element.getAttributes().values()) {
+					for (ElementModel model : models) {
+						if (model.getAttributeValues().containsKey(attribute)) {
+							transitiveClosure.addAttributeValue(model.getAttributeValues().get(attribute));
+						}
 					}
 				}
 			}
 		}
-		
+
 		for (Relationship rel : transitiveClosure.getRelations()) {
-			for (Integer attribute : rel.getAttributes().values()) {
-				for (ElementModel model : models) {
-					if (model.getAttributeValues().containsKey(attribute)) {
-						transitiveClosure.addAttriputeValue(model.getAttributeValues().get(attribute));
+			if (rel.getAttributes().values() != null) {
+				for (Integer attribute : rel.getAttributes().values()) {
+					for (ElementModel model : models) {
+						if (model.getAttributeValues().containsKey(attribute)) {
+							transitiveClosure.addAttributeValue(model.getAttributeValues().get(attribute));
+						}
 					}
 				}
 			}
 		}
 	}
 
-	
 	private void dealWithDuplicatingMocksInTheGraph(Map<String, List<ElementRelationTuple>> graph, Set<String> mocks) {
-		
+
 		for (String mock : mocks) {
-			
+
 			String baseName = mock.substring(0, mock.lastIndexOf('-'));
-			
+
 			if (graph.containsKey(baseName)) {
-				
+
 				Element real = findRequestedElement(graph, baseName);
-				
-//				set mock's relations to the real one
+
+				// set mock's relations to the real one
 				for (ElementRelationTuple tuple : graph.get(mock)) {
-					
+
 					if (tuple.getRelationship() != null) {
-						
+
 						if (tuple.getRelationship().getFromID().equals(mock)) {
-							
-							Relationship rel = new Relationship(tuple.getRelationship().getNameType(), baseName, tuple.getRelationship().getToID());
+
+							Relationship rel = new Relationship(tuple.getRelationship().getNameType(), baseName,
+									tuple.getRelationship().getToID());
 							tuple.setRelationship(rel);
 						} else {
-							
-							Relationship rel = new Relationship(tuple.getRelationship().getNameType(), tuple.getRelationship().getFromID(), baseName);
+
+							Relationship rel = new Relationship(tuple.getRelationship().getNameType(),
+									tuple.getRelationship().getFromID(), baseName);
 							tuple.setRelationship(rel);
 						}
 					}
-					
+
 					graph.get(baseName).add(tuple);
-					
+
 					ElementRelationTuple tupleToBeRemoved = null;
-					
+
 					List<ElementRelationTuple> list = graph.get(tuple.getElement().getNameID());
-					
+
 					for (ElementRelationTuple correspondingTuple : list) {
-						
+
 						if (correspondingTuple.getElement().getNameID().equals(mock)) {
 							tupleToBeRemoved = correspondingTuple;
 							break;
 						}
 					}
-					
+
 					list.remove(tupleToBeRemoved);
-					
+
 					list.add(new ElementRelationTuple(real, tuple.getRelationship()));
 				}
-				
+
 				graph.remove(mock);
 			}
 		}
 	}
 
-	
 	private void partsToGraph(Map<String, List<ElementRelationTuple>> graph, ElementModel model, Set<String> mocks) {
-		
+
 		for (Element element : model.getElements().values()) {
 			if (element.getParts() == null || element.getParts().isEmpty()) {
 				continue;
 			}
-			
+
 			if (!graph.containsKey(element.getNameID())) {
 				graph.put(element.getNameID(), new ArrayList());
-				
+
 				if (element.getNameID().endsWith("mock")) {
 					mocks.add(element.getNameID());
 				}
- 			}
-			
+			}
+
 			for (Parts parts : element.getParts()) {
 				for (String part : parts.getParts()) {
 					if (!graph.containsKey(part)) {
 						graph.put(part, new ArrayList());
-						
+
 						if (part.endsWith("mock")) {
 							mocks.add(part);
 						}
-		 			}
-					
+					}
+
 					ElementRelationTuple tuple = new ElementRelationTuple(model.getElements().get(part));
 					graph.get(element.getNameID()).add(tuple);
-					
+
 					tuple = new ElementRelationTuple(element);
 					graph.get(part).add(tuple);
 				}
@@ -149,76 +152,76 @@ public class TransitiveClosureService {
 		}
 	}
 
-	
-	private void relationsToGraph(Map<String, List<ElementRelationTuple>> graph, ElementModel model, Set<String> mocks) {
-		
+	private void relationsToGraph(Map<String, List<ElementRelationTuple>> graph, ElementModel model,
+			Set<String> mocks) {
+
 		for (Relationship relation : model.getRelations()) {
-			
+
 			if (!graph.containsKey(relation.getFromID())) {
 				graph.put(relation.getFromID(), new ArrayList());
-				
+
 				if (relation.getFromID().endsWith("mock")) {
 					mocks.add(relation.getFromID());
 				}
- 			}
-			
-			ElementRelationTuple tuple = new ElementRelationTuple(model.getElements().get(relation.getToID()), relation);
+			}
+
+			ElementRelationTuple tuple = new ElementRelationTuple(model.getElements().get(relation.getToID()),
+					relation);
 			graph.get(relation.getFromID()).add(tuple);
-			
+
 			if (!graph.containsKey(relation.getToID())) {
 				graph.put(relation.getToID(), new ArrayList());
-				
+
 				if (relation.getToID().endsWith("mock")) {
 					mocks.add(relation.getToID());
 				}
- 			}
-			
+			}
+
 			tuple = new ElementRelationTuple(model.getElements().get(relation.getFromID()), relation);
 			graph.get(relation.getToID()).add(tuple);
 		}
 	}
-	
 
 	public TransitiveClosure getTransitiveClosure(Map<String, List<ElementRelationTuple>> graph, String id, int depth) {
-		
+
 		ElementModel model = new ElementModel();
 		Map<Integer, List<String>> layers = new HashMap();
-		
+
 		Element requested = null;
-		
+
 		if (graph.containsKey(id)) {
 			requested = this.findRequestedElement(graph, id);
 		}
-		
+
 		if (requested == null) {
 			TransitiveClosure trans = new TransitiveClosure();
 			trans.setModel(model);
 			trans.setLayers(layers);
 			return trans;
 		}
-		
+
 		model.addElement(requested);
-		
+
 		Queue<ElementRelationTuple> queue = new LinkedList();
-		
+
 		for (ElementRelationTuple tuple : graph.get(id)) {
 			queue.add(tuple);
 		}
-		
-		this.addElementsAndRelationsToModel(model, queue, new LinkedList<ElementRelationTuple>(), depth, 1, graph, layers);
-		
+
+		this.addElementsAndRelationsToModel(model, queue, new LinkedList<ElementRelationTuple>(), depth, 1, graph,
+				layers);
+
 		TransitiveClosure closure = new TransitiveClosure();
 		closure.setLayers(layers);
 		closure.setModel(model);
-		
+
 		return closure;
 	}
-	
-	
+
 	private Element findRequestedElement(Map<String, List<ElementRelationTuple>> graph, String id) {
-		
+
 		Element element = graph.get(id).get(0).getElement();
-		
+
 		if (element != null) {
 			for (ElementRelationTuple tuple : graph.get(element.getNameID())) {
 				if (tuple.getElement().getNameID().equals(id)) {
@@ -228,19 +231,19 @@ public class TransitiveClosureService {
 		}
 		return null;
 	}
-	
-	
-	private void addElementsAndRelationsToModel(ElementModel model, Queue<ElementRelationTuple> currentLayer, Queue<ElementRelationTuple> nextLayer,
-			int depth, int currentDepth, Map<String, List<ElementRelationTuple>> graph, Map<Integer, List<String>> layers) {
-		
+
+	private void addElementsAndRelationsToModel(ElementModel model, Queue<ElementRelationTuple> currentLayer,
+			Queue<ElementRelationTuple> nextLayer, int depth, int currentDepth,
+			Map<String, List<ElementRelationTuple>> graph, Map<Integer, List<String>> layers) {
+
 		if (currentDepth > depth) {
 			return;
 		} else if (currentLayer.isEmpty()) {
 			return;
 		}
-		
+
 		List<String> layer = new ArrayList();
-		
+
 		while (!currentLayer.isEmpty()) {
 			ElementRelationTuple tuple = currentLayer.poll();
 			if (!model.getElements().containsKey(tuple.getElement().getNameID())) {
@@ -248,7 +251,7 @@ public class TransitiveClosureService {
 				for (ElementRelationTuple nextTuple : graph.get(tuple.getElement().getNameID())) {
 					nextLayer.add(nextTuple);
 				}
-				
+
 				layer.add(tuple.getElement().getNameID());
 			}
 			Relationship relation = tuple.getRelationship();
@@ -258,12 +261,13 @@ public class TransitiveClosureService {
 				}
 			}
 		}
-		
+
 		if (!layer.isEmpty()) {
 			layers.put(currentDepth, layer);
 		}
-		
-		addElementsAndRelationsToModel(model, nextLayer, new LinkedList<ElementRelationTuple>(), depth, currentDepth+1, graph, layers);
+
+		addElementsAndRelationsToModel(model, nextLayer, new LinkedList<ElementRelationTuple>(), depth,
+				currentDepth + 1, graph, layers);
 	}
- 	
+
 }
