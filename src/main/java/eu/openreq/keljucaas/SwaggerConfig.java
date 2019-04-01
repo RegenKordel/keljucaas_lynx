c1ff2ce3d86d0e506d04e4a6238bbc8d86b83aa1
laurtom <tlaurinen@gmail.com>
2019-04-01 14:45:32 +0300

    swagger fix

--- src/main/java/eu/openreq/keljucaas/SwaggerConfig.java
+++ src/main/java/eu/openreq/keljucaas/SwaggerConfig.java
@@ -17,6 +17,7 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+        		.host("api.openreq.eu/keljucaas")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("eu.openreq.keljucaas.controllers"))    
                 .paths(PathSelectors.any())  
